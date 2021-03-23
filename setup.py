989cfc8f7852e2ba463b10568842b53c8afdc37d
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-03-23 18:32:23 +0900

    Create setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+with open("requirements.txt", "r") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+DESCRIPTION = "This is a plotter of Arduino."
+
+setup(
+    name="TLTester",
+    version="0.1.0",
+    author="5yutan5",
+    author_email="4yutan4@gmail.com",
+    description=DESCRIPTION,
+    longdescription=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/5yutan5/tensile-LCR-tester",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    dependency_links=["git@git+https://github.com/pyqtgraph/pyqtgraph"],
+    entry_points={"gui_scripts": ["ArduinoPlotter = main:main"]},
+    python_requires=">=3.9",
+)
